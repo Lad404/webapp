9fe2242035d1cef95ede2f2d444aefbf6c980807
Lad404 <119912787+Lad404@users.noreply.github.com>
2024-09-08 16:42:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World. This is a test commit 2\n');
+    res.end('Hello World. This is a test commit 3\n');
 });
 server.listen(3000, '0.0.0.0', () => {
     console.log('Server running');
