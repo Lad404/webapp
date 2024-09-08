fd9ad27152dca1a780f672643114051e5652229b
Lad404 <119912787+Lad404@users.noreply.github.com>
2024-09-08 18:21:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!!.\n');
+    res.end('Hello World , my name is Sumit!!.\n');
 });
 server.listen(3000, '0.0.0.0', () => {
     console.log('Server running');
